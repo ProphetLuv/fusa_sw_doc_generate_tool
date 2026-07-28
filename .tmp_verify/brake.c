ae7fe0ebe71619515ce0f330b4ec2de42f478ee0
ProphetLuv <32220060+ProphetLuv@users.noreply.github.com>
2026-07-28 13:14:51 +0800

    Delete .tmp_verify directory

--- .tmp_verify/brake.c
+++
@@ -1,4 +0,0 @@
-#include "brake.h"
-static int g_pressure;
-void brake_init(void){ g_pressure = 0; }
-int brake_apply(int level){ if(level<0) return -1; g_pressure = level; return g_pressure; }
--- .tmp_verify/sensor.c
+++
@@ -1,5 +0,0 @@
-#include "sensor.h"
-static int g_raw;
-void sensor_init(void){ g_raw = 0; }
-int sensor_read(void){ return g_raw; }
-void sensor_set(int v){ g_raw = v; }
